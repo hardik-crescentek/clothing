e50cab0abdd687c3f3ecbb33904799a7c5f44ac7
hardik-crescentek <hardik.bhalsod@outrightsolutions.net>
2024-07-02 16:37:56 +0530

    clients add and edit form design page changes add.

--- resources/views/clients/create.blade.php
+++ resources/views/clients/create.blade.php
@@ -41,110 +41,86 @@
 
                 {!! Form::open(array('route' => 'clients.store','method'=>'POST', 'class'=>"form-validate", 'novalidate')) !!}
                 <input type="hidden" name="redirectTo" value="{{$redirect}}">
-                <div class="form-group row d-flex align-items-center mb-5">
-                    <label class="col-lg-3 form-control-label d-flex justify-content-lg-end">Company Name / Shop Name</label>
-                    <div class="col-lg-5">
+                <div class="row">
+                    <div class="form-group col-lg-3">
+                        <label class="form-control-label d-flex">Company Name / Shop Name<span class="text-danger ml-2">*</span></label>
                         {!! Form::text('company_name', null, array('id'=>'company_name','class' => 'form-control' , 'data-validation'=>"required")) !!}
                     </div>
-                </div>
-                <div class="form-group row d-flex align-items-center mb-5">
-                    <label class="col-lg-3 form-control-label d-flex justify-content-lg-end">Owner FirstName</label>
-                    <div class="col-lg-5">
+                    <div class="form-group col-lg-3">
+                        <label class="form-control-label d-flex">Owner FirstName<span class="text-danger ml-2">*</span></label>
                         {!! Form::text('firstname', null, array('placeholder' => 'FirstName','class' => 'form-control', 'data-validation'=>"required")) !!}
                     </div>
-                </div>
-                <div class="form-group row d-flex align-items-center mb-5">
-                    <label class="col-lg-3 form-control-label d-flex justify-content-lg-end">LastName</label>
-                    <div class="col-lg-5">
+                    <div class="form-group col-lg-3">
+                        <label class="form-control-label d-flex">LastName<span class="text-danger ml-2">*</span></label>
                         {!! Form::text('lastname', null, array('placeholder' => 'LastName','class' => 'form-control', 'data-validation'=>"required")) !!}
                     </div>
-                </div>
-                
-                <div class="form-group row d-flex align-items-center mb-5">
-                    <label class="col-lg-3 form-control-label d-flex justify-content-lg-end">Address</label>
-                    <div class="col-lg-5">
+                    <div class="form-group col-lg-3">
+                        <label class="form-control-label d-flex">Address<span class="text-danger ml-2">*</span></label>
                         {!! Form::text('address', null, array('placeholder' => 'Address','class' => 'form-control', 'data-validation'=>"required")) !!}
                     </div>
                 </div>
-                <div class="form-group row d-flex align-items-center mb-5">
-                    <label class="col-lg-3 form-control-label d-flex justify-content-lg-end">City</label>
-                    <div class="col-lg-5">
+
+                <div class="row">
+                    <div class="form-group col-lg-3">
+                        <label class="form-control-label d-flex">City<span class="text-danger ml-2">*</span></label>
                         {!! Form::text('city', null, array('placeholder' => 'City','class' => 'form-control', 'data-validation'=>"required")) !!}
                     </div>
-                </div>
-                <div class="form-group row d-flex align-items-center mb-5">
-                    <label class="col-lg-3 form-control-label d-flex justify-content-lg-end">State</label>
-                    <div class="col-lg-5">
+                    <div class="form-group col-lg-3">
+                        <label class="form-control-label d-flex">State<span class="text-danger ml-2">*</span></label>
                         {!! Form::text('state', null, array('placeholder' => 'State','class' => 'form-control', 'data-validation'=>"required")) !!}
                     </div>
-                </div>
-                <div class="form-group row d-flex align-items-center mb-5">
-                    <label class="col-lg-3 form-control-label d-flex justify-content-lg-end">Country</label>
-                    <div class="col-lg-5">
+                    <div class="form-group col-lg-3">
+                        <label class="form-control-label d-flex">Country<span class="text-danger ml-2">*</span></label>
                         @include('countries', ['default' => null, 'attributes' => array('class' => 'form-control', 'data-validation'=>"required")])
                     </div>
-                </div>
-                <div class="form-group row d-flex align-items-center mb-5">
-                    <label class="col-lg-3 form-control-label d-flex justify-content-lg-end">Zip</label>
-                    <div class="col-lg-5">
+                    <div class="form-group col-lg-3">
+                        <label class="form-control-label d-flex">Zip<span class="text-danger ml-2">*</span></label>
                         {!! Form::text('zip', null, array('placeholder' => 'Zip','class' => 'form-control', 'data-validation'=>"required")) !!}
                     </div>
                 </div>
-                <div class="form-group row d-flex align-items-center mb-5">
-                    <label class="col-lg-3 form-control-label d-flex justify-content-lg-end">Nature Of Business</label>
-                    <div class="col-lg-5">
+
+                <div class="row">
+                    <div class="form-group col-lg-3">
+                        <label class="form-control-label d-flex">Nature Of Business</label>
                         {!! Form::select('business_nature', $business_nature,null, array('class' => 'form-control' , 'id' => 'input_business_nature')) !!}
                     </div>
-                </div>
-                <div class="form-group row mb-5 row_business_nature_other" style="display: none">
-                    <label class="col-lg-3 form-control-label d-flex justify-content-lg-end mt-2">Other Nature Of Business</label>
-                    <div class="col-lg-5">
+                    <div class="form-group col-lg-3 row_business_nature_other" style="display: none">
+                        <label class="form-control-label d-flex">Other Nature Of Business</label>
                         {!! Form::text('business_nature_other', null, array('id'=>'business_nature_other','placeholder' => 'Other Nature Of Business','class' => 'form-control')) !!}
                     </div>
-                </div>
-                <div class="form-group row d-flex align-items-center mb-5">
-                    <label class="col-lg-3 form-control-label d-flex justify-content-lg-end">Date Of Birth</label>
-                    <div class="col-lg-5">
+                    <div class="form-group col-lg-3">
+                        <label class="form-control-label d-flex">Date Of Birth<span class="text-danger ml-2">*</span></label>
                         {!! Form::text('dob', null, array('id'=>'dob','placeholder' => 'Date of Birth','class' => 'form-control', 'data-validation'=>"required")) !!}
                     </div>
-                </div>
-                <div class="form-group row d-flex align-items-center mb-5">
-                    <label class="col-lg-3 form-control-label d-flex justify-content-lg-end">Phone</label>
-                    <div class="col-lg-5">
+                    <div class="form-group col-lg-3">
+                        <label class="form-control-label d-flex">Phone<span class="text-danger ml-2">*</span></label>
                         {!! Form::text('phone', null, array('placeholder' => 'Phone','class' => 'form-control', 'data-validation'=>"required")) !!}
                     </div>
                 </div>
-                <div class="form-group row d-flex align-items-center mb-5">
-                    <label class="col-lg-3 form-control-label d-flex justify-content-lg-end">Email</label>
-                    <div class="col-lg-5">
+
+                <div class="row">
+                    <div class="form-group col-lg-3">
+                        <label class="form-control-label d-flex">Email<span class="text-danger ml-2">*</span></label>
                         {!! Form::text('email', null, array('placeholder' => 'Email','class' => 'form-control', 'data-validation'=>"required")) !!}
                     </div>
-                </div>
-                
-                <div class="form-group row d-flex align-items-center mb-5">
-                    <label class="col-lg-3 form-control-label d-flex justify-content-lg-end">Password</label>
-                    <div class="col-lg-5">
+                    <div class="form-group col-lg-3">
+                        <label class="form-control-label d-flex">Password<span class="text-danger ml-2">*</span></label>
                         {!! Form::password('password', array('placeholder' => 'Password','class' => 'form-control', 'data-validation'=>"required")) !!}
                     </div>
-                </div>
-
-                <div class="form-group row d-flex align-items-center mb-5">
-                    <label class="col-lg-3 form-control-label d-flex justify-content-lg-end">Confirm Password</label>
-                    <div class="col-lg-5">
+                    <div class="form-group col-lg-3">
+                        <label class="form-control-label d-flex">Confirm Password<span class="text-danger ml-2">*</span></label>
                         {!! Form::password('password_confirmation', array('placeholder' => 'Confirm Password','class' => 'form-control', 'data-validation'=>"required")) !!}
                     </div>
-                </div>
-                <div class="form-group row d-flex align-items-center mb-5">
-                    <label class="col-lg-3 form-control-label d-flex justify-content-lg-end"></label>
-                    <div class="col-lg-5">
+                    <div class="form-group col-lg-3 d-flex align-items-center">
                         <div class="form-check">
                             {!! Form::checkbox('newsletter', 1, null ,array('class' => 'form-check-input', 'id' => 'newsletter')) !!}
                             <label for="newsletter" class="form-check-label" for="printWidth">Recive update on new arrivals</label>
                         </div>
                     </div>
                 </div>
-                <div class="form-group row d-flex align-items-center mb-5">
-                    <div class="col-lg-5 offset-lg-3">
+                                
+                <div class="form-group row d-flex align-items-center mt-5">
+                    <div class="col-lg-12 d-flex justify-content-center">
                         <button type="submit" class="btn btn-primary btn-lg">Save</button>
                     </div>
                 </div>
